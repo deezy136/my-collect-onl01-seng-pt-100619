1903d889f05f92ee1f8961a85eaee8a8acee304d
deezy13 <deezy13@gmail.com>
2019-10-15 17:47:02 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,7 +4,7 @@ new_collection = []
 
 i = 0 
 while i < collection.size
-new_collection << yield(collection[i].upcase.first)
+new_collection << yield(collection[i].upcase)
 i+=1
 end 
 new_collection
