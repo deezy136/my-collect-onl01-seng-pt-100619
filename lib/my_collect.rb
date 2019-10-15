a95d33b686a13b3ca8a0081fca45bce122428438
deezy13 <deezy13@gmail.com>
2019-10-15 17:48:56 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,17 @@
 def my_collect(collection)
 
 new_collection = []
+collection_two = []
 
 i = 0 
 while i < collection.size
 new_collection << yield(collection[i].upcase)
 i+=1
 end 
-new_collection
+new_collection.each do | elements|
+  
+ collection_two << elements.first 
+end 
+
+collection_two
 end 
